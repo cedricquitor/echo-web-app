abf1523b3965f13a2b90d2817af5128b330dfd86
cedricquitor <cedricquitor.dev@gmail.com>
2022-04-08 23:58:06 +0800

    Established connection to MongoDB database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+// MongoDB connection
+const CONNECTION_URL = "mongodb+srv://echoadministrator:UXayJfycnWyWOCPn@cluster0.almyt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });

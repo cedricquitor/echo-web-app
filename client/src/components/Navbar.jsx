d1b63ca9c61be4ad3d7b3c671e07b606866b3e33
cedricquitor <cedricquitor.dev@gmail.com>
2022-05-01 06:07:49 +0800

    Added custom fonts and navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="fixed top-0 w-full py-12 bg-transparent shadow-lg">
+      <div className="flex flex-row justify-between container mx-auto">
+        {/* Logo */}
+        <div>
+          <img src="./assets/echo-full.png" alt="echo Logo" className="h-14" />
+        </div>
+        {/* Links */}
+        <div className="flex flex-row my-auto">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Use-case</a>
+          <a href="">Button</a>
+        </div>
+      </div>
     </div>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: "'Raleway', sans-serif",
+        fredoka: "'Fredoka One', cursive",
+      },
+    },
   },
   plugins: [],
 };

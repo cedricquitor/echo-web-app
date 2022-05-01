975b7021507bb7afcf5086f23b31fbcf9d167856
cedricquitor <cedricquitor.dev@gmail.com>
2022-05-01 08:51:05 +0800

    Added main layout

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-const MainLayout = () => {
+const MainLayout = (props) => {
   return (
-    <div>
-      <h1>MainLayout</h1>
+    <div className="bg-[#F0F2F5] flex flex-col justify-between">
+      <Navbar />
+      <div className="my-20">{props.children}</div>
+      <Footer />
     </div>
   );
 };

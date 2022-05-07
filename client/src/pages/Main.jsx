1e521c3992dbd66c446824d2be3de3c0bfd25bb5
cedricquitor <cedricquitor.dev@gmail.com>
2022-05-07 18:11:36 +0800

    Initiated main UI

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div>
-      <h1>Main</h1>
+    <div className="mt-20 flex justify-center items-center">
+      <div className="p-8 bg-white rounded-2xl">
+        {/* User */}
+        <div className="flex">
+          <h1>Image</h1>
+          <h1>Name</h1>
+        </div>
+        <div>
+          <h1>Tell us about your adventure, John!</h1>
+          {/* Text Area */}
+        </div>
+        <div className="flex">
+          <h1>Image Icon</h1>
+          <h1>Tags</h1>
+        </div>
+      </div>
     </div>
   );
 };

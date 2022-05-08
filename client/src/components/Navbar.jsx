7203b96c12894c7b2437977c6c114b881c4a9915
cedricquitor <cedricquitor.dev@gmail.com>
2022-05-08 17:28:57 +0800

    Added styles on scroll in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
 
   const scrollEvent = () => {
-    if (window.scrollY >= 50) {
+    if (window.scrollY >= 200) {
       setIsScrolled(true);
     } else {
       setIsScrolled(false);
@@ -15,14 +15,14 @@ const Navbar = () => {
   window.addEventListener("scroll", scrollEvent);
 
   return (
-    <div className="fixed top-0 w-full py-12 bg-transparent">
+    <div className={isScrolled ? "fixed top-0 w-full py-4 bg-white shadow-xl transition duration-500" : "fixed top-0 w-full py-12 bg-transparent transition duration-500"}>
       <div className="flex flex-row justify-between container mx-auto">
         {/* Logo */}
         <div>
-          <img src="./assets/echo-full.png" alt="echo Logo" className="h-14" />
+          <img src={isScrolled ? "./assets/echo-mini.png" : "./assets/echo-full.png"} alt="echo Logo" className={isScrolled ? "h-10" : "h-14"} />
         </div>
         {/* Links */}
-        <div className="flex flex-row font-raleway text-2xl text-[#727272]">
+        <div className={isScrolled ? "flex flex-row font-raleway text-lg text-[#727272]" : "flex flex-row font-raleway text-2xl text-[#727272]"}>
           <a href="#" className="mr-8 my-auto">
             Home
           </a>
@@ -32,7 +32,7 @@ const Navbar = () => {
           <a href="#" className="mr-8 my-auto">
             Use-case
           </a>
-          <a href="#" className="bg-[#40916C] text-white py-2 px-12 rounded-3xl my-auto">
+          <a href="#" className={isScrolled ? "bg-[#40916C] text-white py-1 px-8 rounded-3xl my-auto" : "bg-[#40916C] text-white py-2 px-12 rounded-3xl my-auto"}>
             App
           </a>
         </div>
